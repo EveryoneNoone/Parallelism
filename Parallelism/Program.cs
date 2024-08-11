36344d5f0c974053b748458a2479f0603a195710
Aleksandr Smirnov <aleksandr.smirnov2009@gmail.com>
2024-08-11 15:15:22 +0200

    Done homework

--- Parallelism/Program.cs
+++ Parallelism/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿using Parallelism;
+using System.Diagnostics;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var path = @"c:\Temp\Files\";
+        var workWithFiles = new WorkWithFiles();
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+        var tasks = workWithFiles.GetDirectoryFiles(path);
+        var whiteSpaceCount = await Task.WhenAll(tasks);
+        Console.WriteLine(whiteSpaceCount.Sum());
+        stopWatch.Stop();
+        Console.WriteLine(stopWatch.Elapsed.ToString());
     }
 }
---
+++ Parallelism/WorkWithFiles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallelism
+{
+    internal class WorkWithFiles
+    {
+        public List<Task<int>> GetDirectoryFiles(string path)
+        {
+            var files = Directory.GetFiles(path, "*.txt");
+            var tasks = files.Select(FileCountWhiteSpace);
+            return tasks.ToList();
+        }
+
+        public async Task<int> FileCountWhiteSpace(string path)
+        {
+            string fileContent = await File.ReadAllTextAsync(path);
+            var whitespaceCount = fileContent.Count(char.IsWhiteSpace);
+            return whitespaceCount;
+        }
+    }
+}
